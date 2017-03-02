52acd256dbf9b63d0117227bc7af3ca8340819c0
Jarrett Smith <Jarrett Smith>
2017-03-01 20:26:03 -0500

    First commit

--- server.rb
+++ server.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 
-get '/' do
-  "Hello world" # <------ Everything on the internet is a string
-end
+# get '/' do
+#   "Hello world" # <------ Everything on the internet is a string
+# end
+#
+# get "/sinatra" do
+#   "Hello Sinatra"
+# end
 
-get "/sinatra" do
-  "Hello Sinatra"
-end
+get '/' do
+
+current_time = Time.now
+
+weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ]
+
+  "Hello! Today is #{weekdays[current_time.wday]}."
+
+  end
